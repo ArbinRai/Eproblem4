d00b966ba4de8b974df25a0d157457c103481b86
Arbin <Arbinkrai@gmail.com>
2015-09-28 14:11:22 -0500

    Just a rough idea

---
+++ src/com/Arbin/Main.java
@@ -0,0 +1,15 @@
+package com.Arbin;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*while(...){
+	String choice=scanner.next();
+	String songName=(String) songs.get(choice);
+	int timesPlayed=(int) timePlay.get(songName);
+	//add 1 to timesPlayed
+	//put songName,timesPlayed in timePlay
+	//Ask if DJ wants to play another song, break; if not
+	 */
+    }
+}
